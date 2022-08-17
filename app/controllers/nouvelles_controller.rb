043a7b54d12b37f2529dbae0ff11b637bef52604
JeanGuillaume Vicente <jg.vicente@wanadoo.fr>
2022-08-17 11:44:40 +0200

    nouvelle model and controller done

---
+++ app/controllers/nouvelles_controller.rb
@@ -0,0 +1,41 @@
+class NouvellesController < ApplicationController
+
+
+  def new
+    @nouvelle = Nouvelle.new
+    authorize @nouvelle
+  end
+
+  def create
+    @nouvelle = Nouvelle.new(nouvelle_params)
+    @nouvelle.user = current_user
+    if @nouvelle.save
+      redirect_to nouvelles_path
+    else
+      render :new
+    end
+    authorize @nouvelle
+  end
+
+  def update
+    @nouvelle.update(nouvelle_params)
+    redirect_to nouvelles_path
+    authorize @nouvelle
+  end
+
+  def destroy
+    @nouvelle.destroy
+    redirect_to nouvelles_path
+    authorize @nouvelle
+  end
+
+  private
+
+  def set_nouvelle
+    @nouvelle = Nouvelle.find(params[:id])
+  end
+
+  def nouvelle_params
+    params.require(:nouvelle).permit(:comment, :user)
+  end
+end
