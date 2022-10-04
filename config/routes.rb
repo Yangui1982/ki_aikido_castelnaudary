01aaa97a30065fb00de3f4b94794142c76a743a5
JeanGuillaume Vicente <jg.vicente@wanadoo.fr>
2022-10-05 00:18:08 +0200

    get responsive

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'nouvelles#index'
   resources :nouvelles, except: [:edit, :update]
+  get "about", to: "pages#about"
+  get "info", to: "pages#info"
+  get "media", to: "pages#media"
+  get "enseignement", to: "pages#enseignement"
+  get "bibliographie", to: "pages#bibliographie"
+  get "articles", to: "pages#articles"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
