ccad47d0c3d6e25c7e79f64476492f709d930a6b
JeanGuillaume Vicente <jg.vicente@wanadoo.fr>
2024-03-25 16:56:50 +0100

    adding rich-text + correcting some bugs

--- db/migrate/20220817092357_create_nouvelles.rb
+++ db/migrate/20220817092357_create_nouvelles.rb
@@ -1,7 +1,6 @@
 class CreateNouvelles < ActiveRecord::Migration[6.1]
   def change
     create_table :nouvelles do |t|
-      t.string :comment
       t.references :user, null: false, foreign_key: true
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,19 @@
 Nouvelle.destroy_all
 User.destroy_all
 
-User.create!(
+user=User.create!(
   email: 'leduc.vicente@free.fr',
   password: 'jeangab11'
+)
+
+Nouvelle.create!(
+  content: 'Saison 2023-2024 : Les cours ont lieu aux dojos : Le lundi soir de 19h30 à 21h30 Au Gymnase Pierre de Coubertin, 4 Rue Jacques Anquetil, 11400 Castelnaudary.
+            Le jeudi soir de 20h00 à 22h00 au Complexe Sportif Le Millénaire, Rue Anatole France, 11400 Castelnaudary.',
+  user: user
+)
+
+Nouvelle.create!(
+  content: "Les cours de Ki-Aïkido ont lieu au Complexe Sportif Le Millénaire ainsi qu'au Gymnase de Coubertin à Castelnaudary.
+            Si vous êtes intéressez, les deux premiers cours sont gratuits.",
+  user: user
 )
